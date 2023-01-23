7f6d510576ff9022162f5361846037ebabf633ab
Luke Brogan <luke.brogan@mend.io>
2023-01-23 18:41:34 +0000

    XSS - SAST MEND

--- src/main/java/org/t246osslab/easybuggy/vulnerabilities/UnrestrictedSizeUploadServlet.java
+++ src/main/java/org/t246osslab/easybuggy/vulnerabilities/UnrestrictedSizeUploadServlet.java
@@ -1,135 +1,93 @@
-package org.t246osslab.easybuggy.vulnerabilities;
+package org.t246osslab.easybuggy.performance;
 
-import java.awt.image.BufferedImage;
-import java.awt.image.WritableRaster;
-import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
+import io.whitesource.cure.Encoder;
 import java.util.Locale;
 
-import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.t246osslab.easybuggy.core.servlets.AbstractServlet;
-import org.t246osslab.easybuggy.core.utils.MultiPartFileUtils;
 
 @SuppressWarnings("serial")
-@WebServlet(urlPatterns = { "/ursupload" })
-@MultipartConfig
-public class UnrestrictedSizeUploadServlet extends AbstractServlet {
-
-    // Name of the directory where uploaded files is saved
-    private static final String SAVE_DIR = "uploadFiles";
+@WebServlet(urlPatterns = { "/createobjects" })
+public class CreatingUnnecessaryObjectsServlet extends AbstractServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         Locale locale = req.getLocale();
-
+        String strNumber = req.getParameter("number");
+        int number = NumberUtils.toInt(strNumber, -1);
         StringBuilder bodyHtml = new StringBuilder();
-        bodyHtml.append("<form method=\"post\" action=\"ursupload\" enctype=\"multipart/form-data\">");
-        bodyHtml.append(getMsg("msg.reverse.color", locale));
-        bodyHtml.append("<br><br>");
-        bodyHtml.append("<input type=\"file\" name=\"file\" size=\"60\" /><br>");
-        bodyHtml.append(getMsg("msg.select.upload.file", locale));
-        bodyHtml.append("<br><br>");
-        bodyHtml.append("<input type=\"submit\" value=\"" + getMsg("label.upload", locale) + "\" />");
-        bodyHtml.append("<br><br>");
-        if (req.getAttribute("errorMessage") != null) {
-            bodyHtml.append(req.getAttribute("errorMessage"));
+        bodyHtml.append("<form action=\"createobjects\" method=\"post\">");
+        bodyHtml.append(getMsg("msg.calc.sym.natural.numbers", locale));
+        bodyHtml.append("<br><br>n = ");
+        if (number > 0) {
+            Encoder.forHtmlContentXss(bodyHtml
+					.append("<input type=\"text\" name=\"number\" size=\"9\" maxlength=\"9\" value=" + strNumber + ">"));
+        } else {
+            bodyHtml.append("<input type=\"text\" name=\"number\" size=\"9\" maxlength=\"9\">");
         }
-        bodyHtml.append(getInfoMsg("msg.note.unrestrictedsizeupload", locale));
+        bodyHtml.append("<br><br>");
+        if (number > 0) {
+            switch (number) {
+            case 1:
+                break;
+            case 2:
+                bodyHtml.append("1 + 2 = ");
+                break;
+            case 3:
+                bodyHtml.append("1 + 2 + 3 = ");
+                break;
+            case 4:
+                bodyHtml.append("1 + 2 + 3 + 4 = ");
+                break;
+            case 5:
+                bodyHtml.append("1 + 2 + 3 + 4 + 5 = ");
+                break;
+            default:
+                bodyHtml.append("1 + 2 + 3 + ... + " + number + " = ");
+                bodyHtml.append("\\(\\begin{eqnarray}\\sum_{ k = 1 }^{ " + number + " } k\\end{eqnarray}\\) = ");
+            }
+        } else {
+            bodyHtml.append("1 + 2 + 3 + ... + n = ");
+            bodyHtml.append("\\(\\begin{eqnarray}\\sum_{ k = 1 }^{ n } k\\end{eqnarray}\\) = ");
+        }
+        if (number >= 1) {
+            long start = System.nanoTime();
+            bodyHtml.append(calcSum1(number));
+            log.info("{} ms", (System.nanoTime() - start) / 1000000f);
+        }
+        bodyHtml.append("<br><br>");
+        bodyHtml.append("<input type=\"submit\" value=\"" + getMsg("label.calculate", locale) + "\">");
+        bodyHtml.append("<br><br>");
+        bodyHtml.append(getInfoMsg("msg.note.createobjects", locale));
         bodyHtml.append("</form>");
-        responseToClient(req, res, getMsg("title.unrestrictedsizeupload.page", locale), bodyHtml.toString());
+
+        responseToClient(req, res, getMsg("title.createobjects.page", locale), bodyHtml.toString());
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
-        Locale locale = req.getLocale();
-
-        // Get absolute path of the web application
-        String appPath = req.getServletContext().getRealPath("");
-
-        // Create a directory to save the uploaded file if it does not exists
-        String savePath = appPath + File.separator + SAVE_DIR;
-        File fileSaveDir = new File(savePath);
-        if (!fileSaveDir.exists()) {
-            fileSaveDir.mkdir();
+    private Long calcSum1(int number) {
+        Long sum = 0L;
+        for (long i = 1; i <= number; i++) {
+            sum += i;
         }
-
-        try {
-            // Save the file
-            final Part filePart = req.getPart("file");
-            String fileName = MultiPartFileUtils.getFileName(filePart);
-            if (StringUtils.isBlank(fileName)) {
-                doGet(req, res);
-                return;
-            } else if (!isImageFile(fileName)) {
-                req.setAttribute("errorMessage", getErrMsg("msg.not.image.file", locale));
-                doGet(req, res);
-                return;
-            }
-            boolean isConverted = MultiPartFileUtils.writeFile(filePart, savePath, fileName);
-
-            // Reverse the color of the upload image
-            if (!isConverted) {
-                isConverted = reverseColor(new File(savePath + File.separator + fileName).getAbsolutePath());
-            }
-
-            StringBuilder bodyHtml = new StringBuilder();
-            if (isConverted) {
-                bodyHtml.append(getMsg("msg.reverse.color.complete", locale));
-                bodyHtml.append("<br><br>");
-                bodyHtml.append("<img src=\"" + SAVE_DIR + "/" + fileName + "\">");
-                bodyHtml.append("<br><br>");
-            } else {
-                bodyHtml.append(getErrMsg("msg.reverse.color.fail", locale));
-            }
-            bodyHtml.append("<INPUT type=\"button\" onClick='history.back();' value=\""
-                    + getMsg("label.history.back", locale) + "\">");
-            responseToClient(req, res, getMsg("title.unrestrictedsizeupload.page", locale), bodyHtml.toString());
-
-        } catch (Exception e) {
-            log.error("Exception occurs: ", e);
+        return sum;
+    }
+/*
+    private long calcSum2(int number) {
+        long sum = 0L;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
         }
+        return sum;
     }
 
-    private boolean isImageFile(String fileName) {
-        return Arrays.asList("png", "gif", "jpg", "jpeg", "tif", "tiff", "bmp").contains(
-                FilenameUtils.getExtension(fileName));
+    private long calcSum3(int number) {
+        return (long) number * (number + 1) / 2;
     }
-
-    // Reverse the color of the image file
-    private boolean reverseColor(String fileName) throws IOException {
-        boolean isConverted = false;
-        try {
-            BufferedImage image = ImageIO.read(new File(fileName));
-            WritableRaster raster = image.getRaster();
-            int[] pixelBuffer = new int[raster.getNumDataElements()];
-            for (int y = 0; y < raster.getHeight(); y++) {
-                for (int x = 0; x < raster.getWidth(); x++) {
-                    raster.getPixel(x, y, pixelBuffer);
-                    pixelBuffer[0] = ~pixelBuffer[0];
-                    pixelBuffer[1] = ~pixelBuffer[1];
-                    pixelBuffer[2] = ~pixelBuffer[2];
-                    raster.setPixel(x, y, pixelBuffer);
-                }
-            }
-            // Output the image
-            ImageIO.write(image, "png", new File(fileName));
-            isConverted = true;
-        } catch (Exception e) {
-            // Log and ignore the exception
-            log.warn("Exception occurs: ", e);
-        }
-        return isConverted;
-    }
+*/
 }
